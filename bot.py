7cefc71d3bb8c10c35abb59c0426ee23cf12b6af
JasonKChow <jason.k.chow@vanderbilt.edu>
2022-12-24 22:18:57 -0600

    new event manager system

--- bot.py
+++ bot.py
@@ -1,166 +1,177 @@
 import discord
 from discord import app_commands
 from discord.ext import commands, tasks
-import datetime
 import os
 import json
 
 
-class EventForm(discord.ui.Modal, title="Creating an event"):
-    def __init__(self, initInteraction: discord.Interaction):
+class EventsView(discord.ui.View):
+    def __init__(self):
         super().__init__()
+        self.eventID = None
 
-        self.initChannel = initInteraction.channel
-        self.linkedChannel = None
-        self.rolePing = None
+    @discord.ui.button(label="Done!", style=discord.ButtonStyle.green, row=4)
+    async def confirm(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        if self.eventID is not None:
+            msg = interaction.guild.get_scheduled_event(self.eventID).url
+        else:
+            msg = "No event selected!"
+        await interaction.response.edit_message(content=msg, view=None)
+        self.stop()
 
-        # Confirm button
-        self.confirmButton = discord.ui.Button(
-            label="Confirm event", style=discord.ButtonStyle.success
+
+class EventsDropdown(discord.ui.Select):
+    def __init__(self):
+        super().__init__(
+            placeholder="Choose an event...",
+            min_values=1,
+            max_values=1,
+            options=[],
+            row=0,
         )
 
-        self.confirmButton.callback = self._confirm_button
+    async def callback(self, interaction: discord.Interaction):
+        # Get event info
+        eventID = int(self.values[0])
+        self.view.eventID = eventID
+        event = interaction.guild.get_scheduled_event(eventID)
+        self.placeholder = event.name
 
-        self.pingModal = EventPingForm()
+        self.view.add_item(RepeatDropdown(eventID))
+        self.view.add_item(MentionDropdown(eventID))
+        self.view.add_item(MentionButton(eventID))
+        # self.view.add_item(self.parentView.mentionUsers)
+        # self.parentView.add_item(self.parentView.done)
+        # self.view.add_item(discord.ui.MentionableSelect())
 
-    name = discord.ui.TextInput(label="Event Name")
-    description = discord.ui.TextInput(
-        label="Event description", style=discord.TextStyle.long, required=False
-    )
-    date = discord.ui.TextInput(
-        label="Date and Time (24hr Eastern)", placeholder="MM/DD/YYYY HH:MM"
-    )
-    duration = discord.ui.TextInput(label="Duration", placeholder="minutes")
-    repeat = discord.ui.TextInput(label="Repeat every X days?", required=False)
+        self.disabled = True
+        await interaction.response.edit_message(
+            content=event.url, view=self.view
+        )
 
-    async def _confirm_button(self, interaction):
-        await interaction.response.send_message(content="Event created!")
-        await interaction.channel.edit(archived=True, locked=True)
 
-        if self.linkedChannel is not None:
-            event = await interaction.guild.create_scheduled_event(
-                name=self.name.value,
-                description=self.description.value,
-                start_time=self.startTime,
-                end_time=self.endTime,
-                entity_type=discord.EntityType.voice,
-                channel=self.linkedChannel,
-            )
+class RepeatDropdown(discord.ui.Select):
+    def __init__(self, eventID):
+        super().__init__(
+            placeholder="Choose a repeat...",
+            options=[
+                discord.SelectOption(label="Never", value="never"),
+                discord.SelectOption(label="Daily", value="daily"),
+                discord.SelectOption(label="Weekly", value="weekly"),
+                discord.SelectOption(label="Monthly", value="monthly"),
+            ],
+        )
+        self.eventID = eventID
+
+    async def callback(self, interaction: discord.Interaction):
+        # Get event info
+        event = interaction.guild.get_scheduled_event(self.eventID)
+        desc = EventDescription(event.description)
+        desc.set_repeat(self.values[0])
+        await event.edit(description=str(desc), channel=event.channel)
+        await interaction.response.edit_message(view=self.view)
+
+
+class MentionDropdown(discord.ui.MentionableSelect):
+    def __init__(self, eventID):
+        super().__init__(
+            placeholder="Ping users/roles...",
+            min_values=0,
+            max_values=25,
+        )
+        self.eventID = eventID
+
+    async def callback(self, interaction: discord.Interaction):
+        # Get event info
+        event = interaction.guild.get_scheduled_event(self.eventID)
+        ids = []
+        for ping in self.values:
+            if isinstance(ping, discord.Role):
+                ids.append("&" + str(ping.id))
+            else:
+                ids.append(str(ping.id))
+
+        desc = EventDescription(event.description)
+        desc.set_mentions(ids, interaction.channel_id)
+        await event.edit(description=str(desc), channel=event.channel)
+        await interaction.response.edit_message(view=self.view)
+
+
+class MentionButton(discord.ui.Button):
+    def __init__(self, eventID):
+        super().__init__(
+            label="Remove pings",
+            style=discord.ButtonStyle.secondary,
+        )
+        self.eventID = eventID
+
+    async def callback(self, interaction: discord.Interaction):
+        # Get event info
+        event = interaction.guild.get_scheduled_event(self.eventID)
+        desc = EventDescription(event.description)
+        desc.set_mentions([], interaction.channel_id)
+        await event.edit(description=str(desc), channel=event.channel)
+        await interaction.response.edit_message(view=self.view)
+
+
+class EventDescription:
+    def __init__(self, description) -> None:
+        self.header = "#!raided"
+
+        # Check if description has header
+        if description is not None:
+            description = description.split(self.header)
         else:
-            event = await interaction.guild.create_scheduled_event(
-                name=self.name.value,
-                description=self.description.value,
-                start_time=self.startTime,
-                end_time=self.endTime,
-                entity_type=discord.EntityType.external,
-                location="Unknown",
-            )
+            description = [""]
 
-        # Write event to json
-        with open("events.json", "r") as f:
-            events = json.load(f)
-
-        if not str(interaction.guild.id) in events.keys():
-            events[str(interaction.guild.id)] = []
-        eventDict = {
-            "id": event.id,
-            "name": self.name.value,
-            "description": self.description.value,
-            "startTime": self.startTime.timestamp(),
-            "endTime": self.endTime.timestamp(),
-            "channel": self.linkedChannel.id
-            if self.linkedChannel is not None
-            else None,
-            "repeat": self.repeatDays,
-            "repeated": False,
-            "initChannel": self.initChannel.id,
-            "rolePing": self.rolePing if self.rolePing is not None else None,
-            "rolePingMinutes": int(self.pingModal.pingMinutes.value)
-            if self.pingModal.pingMinutes.value is not None
-            else None,
-            "rolePingMessage": self.pingModal.pingMessage.value,
-            "pinged": False,
-            "interestPMMessage": "",
-            "pmed": False,
+        self.description = description[0].rstrip()
+        self.params = {
+            "repeat": None,
+            "mentions": None,
+            "channel": None,
         }
-        events[str(interaction.guild.id)].append(eventDict)
-        with open("events.json", "w") as f:
-            json.dump(events, f)
+        if len(description) == 2:
+            # Split params and fill in dictionary
+            params = description[1].split("\n")
+            for param in params:
+                if param.startswith("#repeat="):
+                    self.params["repeat"] = param.split("=")[1]
+                elif param.startswith("#pingUsers="):
+                    self.params["mentions"] = param.split("=")[1].split(",")
+                elif param.startswith("#channel="):
+                    self.params["channel"] = param.split("=")[1]
 
-    async def _channel_picker_changed(self, interaction):
-        self.linkedChannel = discord.Object(interaction.data["values"][0])
-        await interaction.response.defer()
-
-    async def _role_picker_changed(self, interaction):
-        self.rolePing = interaction.data["values"][0]
-        await interaction.response.send_modal(self.pingModal)
-
-    async def on_submit(self, interaction):
-        # Parse inputs
-        if not self.repeat.value == "":
-            self.repeatDays = int(self.repeat.value)
-        else:
-            self.repeatDays = None
-        self.startTime = datetime.datetime.strptime(
-            self.date.value + "-0400", "%m/%d/%Y %H:%M%z"
-        )
-        self.endTime = self.startTime + datetime.timedelta(
-            minutes=int(self.duration.value)
-        )
-
-        await interaction.response.send_message(
-            f"Creating event: {self.name.value}"
-        )
-        self.optionThread = await interaction.channel.create_thread(
-            name=f"Event options for {self.name.value}",
-            message=interaction.channel.last_message,
-            type=discord.ChannelType.public_thread,
-            auto_archive_duration=60,
-        )
-        await self.optionThread.add_user(interaction.user)
-
-        optionsView = discord.ui.View()
-        # Channel picker
-        channelPicker = discord.ui.Select(
-            placeholder="Connect event to a channel...",
-            options=[discord.SelectOption(label="None", value="none")],
-        )
-        for channel in interaction.guild.channels:
-            if isinstance(channel, discord.VoiceChannel):
-                channelPicker.append_option(
-                    discord.SelectOption(label=channel.name, value=channel.id)
-                )
-        channelPicker.callback = self._channel_picker_changed
-        optionsView.add_item(channelPicker)
-
-        # Role ping picker
-        rolePicker = discord.ui.Select(
-            placeholder="Role to send a message to...",
-            options=[discord.SelectOption(label="None", value="none")],
-        )
-        for role in interaction.guild.roles:
-            rolePicker.append_option(
-                discord.SelectOption(label=role.name, value=role.id)
+    def set_repeat(self, repeat: str):
+        if repeat not in ["never", "daily", "weekly", "monthly"]:
+            raise ValueError(
+                "Repeat must be one of 'never', 'daily', 'weekly', 'monthly'"
             )
-        rolePicker.callback = self._role_picker_changed
-        optionsView.add_item(rolePicker)
 
-        # Confirm button
-        optionsView.add_item(self.confirmButton)
+        self.params["repeat"] = repeat if repeat != "never" else None
+        return str(self)
 
-        await self.optionThread.send(content="Event options", view=optionsView)
+    def set_mentions(self, mentions: list, channel: int):
+        if len(mentions) == 0:
+            self.params["mentions"] = None
+            self.params["channel"] = None
+        else:
+            self.params["mentions"] = ",".join(mentions)
+            self.params["channel"] = channel
 
+        return str(self)
 
-class EventPingForm(discord.ui.Modal, title="Notification Options"):
-    pingMessage = discord.ui.TextInput(
-        label="Message to ping", style=discord.TextStyle.long, required=True
-    )
-    pingMinutes = discord.ui.TextInput(label="Ping X minutes before start")
+    def __str__(self) -> str:
+        description = self.description
+        params = f"\n\n\n\n\n{self.header}\n"
+        for key, value in self.params.items():
+            if value is not None:
+                params += f"#{key}={value}\n"
+        if not params.endswith(f"{self.header}\n"):
+            description += params
 
-    async def on_submit(self, interaction):
-        # Checks if intable
-        int(self.pingMinutes.value)
-        await interaction.response.defer()
+        return description
 
 
 class EventManager(commands.Cog):
@@ -177,69 +188,23 @@ class EventManager(commands.Cog):
 
         self.check_events.start()
 
-    @app_commands.command(name="create_event", description="Create an event.")
-    async def createevent(self, interaction: discord.Interaction):
-        await interaction.response.send_modal(
-            EventForm(initInteraction=interaction)
-        )
+    @app_commands.command(name="edit_events", description="Edit an event.")
+    async def editevent(self, interaction: discord.Interaction):
+        events = interaction.guild.scheduled_events
+        events = {event.id: event.name for event in events}
 
-    @app_commands.command(name="delete_event", description="Delete an event.")
-    async def deleteevent(self, interaction: discord.Interaction):
-        # Get list of events managed by bot
-        self._updateEvents()
-
-        if str(interaction.guild.id) not in self.events.keys():
-            await interaction.response.send_message(
-                "No events are currently managed by this bot in this server.",
-                ephemeral=True,
+        eventsView = EventsView()
+        # Channel picker
+        eventPicker = EventsDropdown()
+        for eventID, eventName in events.items():
+            eventPicker.append_option(
+                discord.SelectOption(label=eventName, value=eventID)
             )
-        else:
-            # Get list of events
-            eventList = self.events[str(interaction.guild.id)]
+        eventsView.add_item(eventPicker)
 
-            eventsView = discord.ui.View()
-            # Channel picker
-            eventPicker = discord.ui.Select(
-                placeholder="Select event to delete...",
-                options=[discord.SelectOption(label="None", value="none")],
-            )
-            for event in eventList:
-                eventPicker.append_option(
-                    discord.SelectOption(
-                        label=event["name"], value=event["id"]
-                    )
-                )
-            eventPicker.callback = self._delete_event
-            eventsView.add_item(eventPicker)
-            await interaction.response.send_message(
-                view=eventsView, ephemeral=True
-            )
-
-    async def _delete_event(self, interaction):
-        guildEvents = self.events[str(interaction.guild.id)]
-        eventID = [
-            event
-            for event in guildEvents
-            if str(event["id"]) == interaction.data["values"][0]
-        ][0]["id"]
-
-        # Update guild events to remove event
-        guildEvents = [
-            event for event in guildEvents if event["id"] != eventID
-        ]
-        self.events[str(interaction.guild.id)] = guildEvents
-
-        # Save to json
-        with open("events.json", "w") as f:
-            f.write(json.dumps(self.events))
-
-        # Delete event in Discord
-        event = interaction.guild.get_scheduled_event(eventID)
-        await event.delete()
         await interaction.response.send_message(
-            "Event deleted.", ephemeral=True
+            view=eventsView, ephemeral=True
         )
-        await interaction.delete_original_message()
 
     def _updateEvents(self):
         with open("events.json", "r") as f:
